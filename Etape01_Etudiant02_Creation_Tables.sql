137d31b2e466771914ee5c576980e47fc057ac07
Olivier <julien.o@live.ca>
2021-07-02 11:53:31 -0400

    Retrait du drop database etudiant 2 etape 1

--- Etape01_Etudiant02_Creation_Tables.sql
+++ Etape01_Etudiant02_Creation_Tables.sql
@@ -5,8 +5,8 @@
  
  Date  02-07-2020                                            
 */
-DROP DATABASE IF EXISTS PosteClientDFC;
-CREATE DATABASE IF NOT EXISTS PosteClientDFC;
+-- DROP DATABASE IF EXISTS PosteClientDFC;
+-- CREATE DATABASE IF NOT EXISTS PosteClientDFC;
 
 USE PosteClientDFC;
 -- -----------------------------------------------------
