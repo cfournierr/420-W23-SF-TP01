c9f733be459b164bf59f4c98a724a95fbc9d7075
Olivier <julien.o@live.ca>
2021-07-06 13:49:46 -0400

    Changement table not null et null

--- Etape01_Etudiant02_Creation_Tables.sql
+++ Etape01_Etudiant02_Creation_Tables.sql
@@ -9,7 +9,7 @@
 
 
 -- DROP DATABASE IF EXISTS PosteClientDFC;
--- CREATE DATABASE IF NOT EXISTS PosteClientDFC;
+CREATE DATABASE IF NOT EXISTS PosteClientDFC;
 
 
 USE PosteClientDFC;
@@ -20,7 +20,7 @@ USE PosteClientDFC;
 CREATE TABLE logiciel(
 	id_logiciel INT UNSIGNED NOT NULL,
     nom_logiciel VARCHAR(45) NOT NULL,
-    nb_license INT UNSIGNED NOT NULL,
+    nb_license INT UNSIGNED,
     id_editeur INT UNSIGNED NOT NULL,
     PRIMARY KEY(id_logiciel)
 
@@ -54,11 +54,27 @@ CREATE TABLE usager_has_poste(
 	id_usager INT UNSIGNED NOT NULL,
     id_poste INT UNSIGNED NOT NULL
     )ENGINE = InnoDB;
-    
+ 
+ /*
+ 
+ALTER TABLE poste_has_logiciel
+ADD PRIMARY KEY(id_poste,id_logiciel);
+
+ALTER TABLE poste_has_logiciel
+ADD FOREIGN KEY(id_logiciel)references logiciel(id_logiciel);
+
+ALTER TABLE poste_has_logiciel
+ADD FOREIGN KEY (id_poste)references poste(id_poste);
+
+SELECT* FROM poste_has_logiciel as pl
+LEFT JOIN poste as p
+ON p.id_poste = pl.id_poste;
+
+SELECT*FROM poste;
 
  ALTER TABLE usager
  ADD FOREIGN KEY (local_id_local)REFERENCES local(id_local)
- 
+ */
 
 
 
