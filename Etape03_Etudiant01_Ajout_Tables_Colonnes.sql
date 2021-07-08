43ddfc495706e146c088105fc92f612f63d2f57a
Crystal Fournier <cfournierr@gmail.com>
2021-07-08 10:14:18 -0400

    ajout de tables, fk et contraintes

--- Etape03_Etudiant01_Ajout_Tables_Colonnes.sql
+++ Etape03_Etudiant01_Ajout_Tables_Colonnes.sql
@@ -1,12 +1,40 @@
 /* 
  Travail pratique 1
- --> Nom de la première personne
- Nom de la deuxième personne
+ --> Crystal Fournier
+ Olivier Julien
  
- Date  JJ-MM-AAAA                                             
+ Date  06-07-2021                                             
 */
 
 USE PosteClientDFC;
 -- -----------------------------------------------------
 -- Étape 3 : Ajout de tables et de colonnes           --
 -- -----------------------------------------------------
+
+CREATE TABLE type_peripherique(
+		id_type_peripherique INT NOT NULL PRIMARY KEY,
+        nom_type_peripherique VARCHAR(25)
+        ) ENGINE = InnoDB;
+
+ALTER TABLE peripherique
+ADD id_type_peripherique INT,
+ADD FOREIGN KEY(id_type_peripherique) REFERENCES type_peripherique(id_type_peripherique);
+
+ALTER TABLE local
+ADD nb_bureau INT,
+ADD nb_prise_ethernet INT,
+ADD imprimante_id_peripherique INT UNSIGNED;
+
+ALTER TABLE local
+ADD FOREIGN KEY(imprimante_id_peripherique)REFERENCES peripherique(id_peripherique);
+
+ALTER TABLE local
+ADD CONSTRAINT nom_local_unique UNIQUE (nom_local);
+
+ALTER TABLE fabricant
+ADD CONSTRAINT nom_fabricant_unique UNIQUE (nom_fabricant);
+
+
+
+
+
