4eb61251427a5661aa55c57a8a81e5feb5071dc9
Olivier <julien.o@live.ca>
2021-07-08 10:03:00 -0400

    ajout table usager modification contrainte

--- Etape03_Etudiant02_Ajout_Tables_Colonnes.sql
+++ Etape03_Etudiant02_Ajout_Tables_Colonnes.sql
@@ -1,12 +1,41 @@
 /* 
  Travail pratique 1
  Nom de la première personne
- --> Nom de la deuxième personne
+ --> Olivier Julien
  
- Date  JJ-MM-AAAA                                             
+ Date  06-07-2021                                             
 */
 
 USE PosteClientDFC;
 -- -----------------------------------------------------
 -- Étape 3 : Ajout de tables et de colonnes           --
 -- -----------------------------------------------------
+
+CREATE TABLE typeUsager(
+	id_typeUsager INT NOT NULL,
+    nom_statut VARCHAR(45),
+    PRIMARY KEY (id_typeUsager)
+    )ENGINE=INNODB;
+
+
+ALTER TABLE usager
+ADD id_typeUsager INT,
+ADD FOREIGN KEY(id_typeUsager)REFERENCES typeUsager(id_typeUsager);
+
+
+ALTER TABLE logiciel
+Modify nom_logiciel VARCHAR(45) UNIQUE NOT NULL;
+
+ALTER TABLE editeur
+MODIFY nom_editeur VARCHAR(45) UNIQUE NOT NULL;
+
+
+
+
+
+
+
+
+
+
+
